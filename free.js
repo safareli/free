24aae7e07f4aab2fb01e1aa6afcc09bda47f75bb
Irakli Safareli <i.safareli@gmail.com>
2016-07-04 04:56:35 +0400

    style(lint): update code to follow current eslint configuration
    closes #3

--- free.js
+++ free.js
@@ -1,25 +1,26 @@
-const daggy = require("daggy")
+const daggy = require('daggy')
 
 if (Function.prototype.map == null) {
-  Object.defineProperty(Function.prototype, 'map',{
+  // eslint-disable-next-line no-extend-native
+  Object.defineProperty(Function.prototype, 'map', {
     value: function(g) {
       const f = this
       return function(x) { return g(f(x)) }
     },
     writable: true,
     configurable: true,
-    enumerable: false
+    enumerable: false,
   })
 }
 
-const compose = (f,g) => (x) => f(g(x))
+const compose = (f, g) => (x) => f(g(x))
 const map = (f) => (v) => v.map(f)
 const id = x => x
 
 const Free = daggy.taggedSum({
   Pure: ['x'],
   Lift: ['x', 'f'],
-  Ap:   ['x', 'y'],
+  Ap: ['x', 'y'],
   Join: ['x'],
 })
 
@@ -29,10 +30,10 @@ Free.Lift.toString = () => 'Free.Lift'
 Free.Join.toString = () => 'Free.Join'
 Free.prototype.toString = function() {
   return this.cata({
-    Pure: (x)    => `Pure(${x})`,
+    Pure: (x) => `Pure(${x})`,
     Lift: (x, f) => `Lift(${x},${f})`,
-    Ap:   (x, y) => `Ap(${x},${y})`,
-    Join: (x)    => `Join(${x})`,
+    Ap: (x, y) => `Ap(${x},${y})`,
+    Join: (x) => `Join(${x})`,
   })
 }
 
@@ -41,28 +42,28 @@ Free.liftF = command => Free.Lift(command, id)
 
 Free.prototype.map = function(f) {
   return this.cata({
-    Pure: (x)    => Free.Pure(f(x)),
-    Lift: (x, g) => Free.Lift(x, compose(f,g)),
-    Ap:   (x, y) => Free.Ap(x, y.map(map(f))),
-    Join: (x)    => Free.Join(x.map(map(f))),
+    Pure: (x) => Free.Pure(f(x)),
+    Lift: (x, g) => Free.Lift(x, compose(f, g)),
+    Ap: (x, y) => Free.Ap(x, y.map(map(f))),
+    Join: (x) => Free.Join(x.map(map(f))),
   })
 }
 
 Free.prototype.ap = function(y) {
   return this.cata({
     Pure: (f) => y.map(f),
-    Ap:   ()  => Free.Ap(y, this),
-    Lift: ()  => Free.Ap(y, this),
-    Join: ()  => Free.Ap(y, this),
+    Ap: () => Free.Ap(y, this),
+    Lift: () => Free.Ap(y, this),
+    Join: () => Free.Ap(y, this),
   })
 }
 
 Free.prototype.chain = function(f) {
   return this.cata({
     Pure: (x) => f(x),
-    Ap:   ()  => Free.Join(this.map(f)),
-    Lift: ()  => Free.Join(this.map(f)),
-    Join: ()  => Free.Join(this.map(f)),
+    Ap: () => Free.Join(this.map(f)),
+    Lift: () => Free.Join(this.map(f)),
+    Join: () => Free.Join(this.map(f)),
   })
 }
 
@@ -76,10 +77,10 @@ Free.prototype.retract = function(of) {
 
 Free.prototype.foldMap = function(f, of) {
   return this.cata({
-    Pure: (x)    => of(x),
+    Pure: (x) => of(x),
     Lift: (x, g) => f(x).map(g),
-    Ap:   (x, y) => y.foldMap(f, of).ap(x.foldMap(f, of)),
-    Join: (x)  => x.map((a) => a.foldMap(f, of)).foldMap(f, of).chain(id),
+    Ap: (x, y) => y.foldMap(f, of).ap(x.foldMap(f, of)),
+    Join: (x) => x.map((a) => a.foldMap(f, of)).foldMap(f, of).chain(id),
   })
 }
 
